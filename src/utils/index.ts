ef09b0ebedf775f3825b217744b9b7b13509dfb3
anton-glebov <anton.glebov@useinsider.com>
2024-02-13 02:40:09 +0300

    feat: add validator

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export const validateUUID = (uuid: string): boolean => {
+    // Regular expression to match UUID format
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(uuid);
+};
