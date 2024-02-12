0abbcfb2c7ef55abe7e33292a703126ec7c71256
anton-glebov <anton.glebov@useinsider.com>
2024-02-13 02:53:00 +0300

    feat: fix eslint

--- src/db.ts
+++ src/db.ts
@@ -4,35 +4,35 @@ import { User } from './types';
 let users: User[] = [];
 
 export const getUsers = (): User[] => {
-    return users;
+  return users;
 };
 
 export const getUserById = (userId: string): User | undefined => {
-    return users.find(user => user.id === userId);
+  return users.find((user) => user.id === userId);
 };
 
 export const createUser = (username: string, age: number, hobbies: string[]): User => {
-    const newUser: User = {
-        id: uuidv4(),
-        username,
-        age,
-        hobbies
-    };
-    users.push(newUser);
-    return newUser;
+  const newUser: User = {
+    id: uuidv4(),
+    username,
+    age,
+    hobbies,
+  };
+  users.push(newUser);
+  return newUser;
 };
 
 export const updateUser = (userId: string, newData: Partial<User>): User | undefined => {
-    const index = users.findIndex(user => user.id === userId);
-    if (index !== -1) {
-        users[index] = { ...users[index], ...newData };
-        return users[index];
-    }
-    return undefined;
+  const index = users.findIndex((user) => user.id === userId);
+  if (index !== -1) {
+    users[index] = { ...users[index], ...newData };
+    return users[index];
+  }
+  return undefined;
 };
 
 export const deleteUser = (userId: string): boolean => {
-    const initialLength = users.length;
-    users = users.filter(user => user.id !== userId);
-    return users.length !== initialLength;
+  const initialLength = users.length;
+  users = users.filter((user) => user.id !== userId);
+  return users.length !== initialLength;
 };
--- src/server.ts
+++ src/server.ts
@@ -5,35 +5,33 @@ import cluster from 'node:cluster';
 import { availableParallelism } from 'node:os';
 dotenv.config();
 
-const CLUSTER = process.env.CLUSTER
+const CLUSTER = process.env.CLUSTER;
 const PORT = process.env.PORT || 3000;
 
 if (CLUSTER) {
-    if (cluster.isPrimary) {
-        // If the current process is the primary process (equivalent to cluster.isMaster)
-        const numCPUs = availableParallelism() - 1;
+  if (cluster.isPrimary) {
+    // If the current process is the primary process (equivalent to cluster.isMaster)
+    const numCPUs = availableParallelism() - 1;
 
-        for (let i = 0; i < numCPUs; i++) {
-            cluster.fork();
-        }
-
-        cluster.on('exit', (worker, code, signal) => {
-            console.log(`Worker ${worker.process.pid} died`);
-            cluster.fork();
-        });
-    } else {
-        const server = http.createServer(userRoutes);
-
-        server.listen(PORT, () => {
-            console.log(`Worker ${process.pid} is running on port ${PORT}`);
-        });
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
     }
-} else {
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+      cluster.fork();
+    });
+  } else {
     const server = http.createServer(userRoutes);
 
     server.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+      console.log(`Worker ${process.pid} is running on port ${PORT}`);
     });
+  }
+} else {
+  const server = http.createServer(userRoutes);
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
-
-
